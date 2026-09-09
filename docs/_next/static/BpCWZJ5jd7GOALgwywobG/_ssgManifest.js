ae2d8ef12054df339be09f4375c2c8bf66de3631
leadfair433-lab <leadfair433@gmail.com>
2026-09-09 12:11:05 +0800

    Fix Vietnamese layout overflow and terminology

---
+++ docs/_next/static/BpCWZJ5jd7GOALgwywobG/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Farticles","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fproducts\u002F[slug]","\u002Fblog\u002F[slug]","\u002Fproducts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
